5f03337dc7d42abbcf5de24de1fdd215addfdb18
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2022-09-05 14:12:38 -0300

    Simple application

---
+++ src/main/scala/robinho/part2datastreams/EssentialStreams.scala
@@ -0,0 +1,27 @@
+package robinho.part2datastreams
+
+import org.apache.flink.streaming.api.scala._
+
+object EssentialStreams {
+
+  def applicationTemplate(): Unit = {
+    // 1 - execution environment
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    // in between, add any sort of computations
+    import org.apache.flink.streaming.api.scala._
+    val simpleNumberStream: DataStream[Int] = env.fromElements(1, 2, 3, 4)
+
+    // perform some actions
+    simpleNumberStream.print()
+
+    // at the end
+    env.execute
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    applicationTemplate()
+  }
+
+}
