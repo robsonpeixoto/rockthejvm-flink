e896ef70f8395a315dfbbcd6a6b49ea47f86cbf6
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2022-10-28 15:51:38 -0300

    Refactor to use a RichSourceFunction to generate random numbers

--- src/main/scala/robinho/part4io/CustomSources.scala
+++ src/main/scala/robinho/part4io/CustomSources.scala
@@ -1,13 +1,14 @@
 package robinho.part4io
 
-import org.apache.flink.streaming.api.functions.source.SourceFunction
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.source.{RichSourceFunction, SourceFunction}
 import org.apache.flink.streaming.api.scala._
 
 import scala.util.Random
 
 object CustomSources {
   // source of numbers, randomly generated
-  class RandomNumberGeneratorSource(minEventsPerSeconds: Double) extends SourceFunction[Long] {
+  class RandomNumberGeneratorSource(minEventsPerSeconds: Double) extends RichSourceFunction[Long] {
     // create local fields/methods
     val maxSleepTime = (1000 / minEventsPerSeconds).toLong
     var isRunning = true
@@ -29,6 +30,14 @@ object CustomSources {
     // contract: the run method shoould stop immediately
     override def cancel(): Unit =
       isRunning = false
+
+    // capability of lifecycle methods - initialize state ...
+    override def open(parameters: Configuration): Unit =
+      println(s"[${Thread.currentThread().getName}] starting source function")
+    override def close(): Unit =
+    println(s"[${Thread.currentThread().getName}] closing source function")
+
+    // can hold state - ValueState, ListState, MapState
   }
 
   def demoSourceFunction(): Unit = {
